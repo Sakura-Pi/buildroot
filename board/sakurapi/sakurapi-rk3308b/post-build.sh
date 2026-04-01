1e9e918ba64635eea3f01ee4b9fda58d4132ce4b
TheSnowfield <17957399+TheSnowfield@users.noreply.github.com>
2026-04-01 17:19:32 +0800

    board: sakurapi-rk3308b fix broken bcm43455

--- board/sakurapi/sakurapi-rk3308b/post-build.sh
+++ board/sakurapi/sakurapi-rk3308b/post-build.sh
@@ -2,3 +2,9 @@
 
 BOARD_DIR=$(dirname "$0")
 install -m 0644 -D "$BOARD_DIR/extlinux.conf" "$BINARIES_DIR/extlinux/extlinux.conf"
+
+# AP6255 Bluetooth firmware: btbcm driver looks in /lib/firmware/brcm/
+if [ -f "$TARGET_DIR/lib/firmware/BCM4345C0.hcd" ] && \
+   [ ! -e "$TARGET_DIR/lib/firmware/brcm/BCM4345C0.hcd" ]; then
+	ln -sf ../BCM4345C0.hcd "$TARGET_DIR/lib/firmware/brcm/BCM4345C0.hcd"
+fi
